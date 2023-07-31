dd5997cdcccce47df4e14fc18f230b0056e8363c
Suphakit P <me@suphakit.net>
2023-08-01 00:32:15 +0700

    feat<page>: about page!

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	import TW_CLASS from '../../constants/tailwind_class'
+</script>
+
+<section>
+	<div class={TW_CLASS.container}>
+		<div>
+			<h1 class={`${TW_CLASS.heading.xxxl} !text-5xl`}>About</h1>
+			<p>
+				This is one of various project that will be come out later in the name of "<a
+					href="https://github.com/Synclarity"
+					target="_blank">Synclarity</a
+				>" in MIT LICENSE & Open source.
+			</p>
+			<p>
+				This project is got inspiration from <a
+					href="https://r.3v.fi/discord-timestamps/"
+					target="_blank">https://r.3v.fi/discord-timestamps/</a
+				> which is used to generating timestamp for discord.
+        If you want to update anything its okay to send a pull request to the repository :)
+			</p>
+
+      <div>
+        <h3 class={`${TW_CLASS.heading.xxl}`}>
+          Links
+        </h3>
+        <div>
+          <a href="https://github.com/Synclarity/synclarity-timestamp">
+            Github Repository
+          </a>
+        </div>
+      </div>
+		</div>
+	</div>
+</section>
