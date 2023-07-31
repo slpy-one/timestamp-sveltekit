d43657b935ad61d879cd67f72267018f4183d5c7
Suphakit P <me@suphakit.net>
2023-07-31 20:43:23 +0700

    feat<module>: setting up handling module

--- src/modules/changeData.ts
+++ src/modules/changeData.ts
@@ -1,10 +1,53 @@
 import type { DateInterface } from '../interface/time'
+import {
+	defaultDateType,
+	longDateTimeType,
+	longTimeType,
+	shortDateTimeType,
+	shortTimeType
+} from './datingFunction'
 
 export default function handlingDate(handlingInput: DateInterface) {
 	if (handlingInput.type !== undefined) {
-		if (handlingInput.date !== undefined || handlingInput.date !== 0) {
-			if (handlingInput.time !== undefined || handlingInput.time !== 0) {
-				return 1
+		if (handlingInput.date !== undefined) {
+			if (handlingInput.time !== undefined) {
+				const date: number = Math.floor(
+					new Date(`${handlingInput.date} ${handlingInput.time}`).getTime() / 1000
+				)
+
+				let returnData: string | undefined = undefined
+
+				switch (handlingInput.type) {
+					case 2:
+						returnData = shortTimeType(date)
+						break
+
+					case 3:
+						returnData = longTimeType(date)
+						break
+
+					case 4:
+						returnData = shortTimeType(date)
+						break
+
+					case 5:
+						returnData = longTimeType(date)
+						break
+
+					case 6:
+						returnData = shortDateTimeType(date)
+						break
+
+					case 7:
+						returnData = longDateTimeType(date)
+						break
+
+					default:
+						returnData = defaultDateType(date)
+						break
+				}
+
+				return returnData
 			} else {
 				return ''
 			}
